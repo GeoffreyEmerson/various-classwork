7c20148c94143f07cc9a97c28d06354c85320cbf
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-07 15:38:03 -0700

    fix sandbox four

--- reading04_sandbox/script.js
+++ reading04_sandbox/script.js
@@ -3,7 +3,8 @@
 // Add and remove DOM element attributes.
 
 $(document).ready(function() {
-  $('p').click(function() { // Select the p element and watch for a click.
-    $(this).toggleClass('green'); // Toggle the green class when clicked.
+  $('p').click(function() { // Select p elements and watch for a click.
+    $('[hidden]').removeAttr('hidden'); // Remove the hidden attribute of anything hidden.
+    $(this).attr('hidden', 'hidden'); // Make the clicked item hidden.
   });
 });
