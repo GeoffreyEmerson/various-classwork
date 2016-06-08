fed1edf9e0bac668171933ca843bb0ae5c78fddc
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-07 19:10:35 -0700

    Fix Eight

--- reading08_sandbox/script.js
+++ reading08_sandbox/script.js
@@ -5,12 +5,12 @@
 $(document).ready(function() {
   $('#one').click(function() { // Waits for a click on list one
     $('li').each(function(){   // Iterates over each list item
-      $(this).appendTo('#two');// Moves it to the other list
+      $('#two').append(this);  // Moves it to the other list
     });
   });
   $('#two').click(function() { // Waits for a click on list two
     $('li').each(function(){   // Iterates over each list item
-      $(this).appendTo('#one');// Moves it to the other list
+      $('#one').append(this);  // Moves it to the other list
     });
   });
 });
