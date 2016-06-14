6cdfd8fe3af9564da36d70e46e636b81065d8789
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-14 11:12:20 -0700

    fix sandbox twelve

--- arrays-and-closures/app12/script.js
+++ arrays-and-closures/app12/script.js
@@ -2,15 +2,14 @@
 
 // [+1] A higher order function (HOF) that has a function F as a parameter, and calls F. Let F accept one parameter. Your app should call the HOF.
 
-function hof(secret) {
-  var closedFunc = function(val) {
-    return val * secret;
+function HOF(val) {
+  var F = function() {
+    return val * 10;
   };
-  return closedFunc;
+  return F();
 }
 
 $(function() {
-  var newFunc = hof(10);
-  var result = newFunc(5);
+  var result = HOF(5);
   console.log(result);
 });
